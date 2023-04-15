7a288d8927b33133e8f467d2dff2a53cd17ad0d0
Alexy Yurchanka <savage2000@tut.by>
2023-04-15 10:24:20 +0200

    Popover 1

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -4,6 +4,9 @@ sap.ui.define(
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
+    "sap/m/ResponsivePopover",
+    "sap/m/Button",
+    "sap/ui/model/BindingMode"
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -12,16 +15,17 @@ sap.ui.define(
   /* oDataModel: { globals:{ specialActivity:85 }, dispOptions:{ internalOnly:false, isExtendedDelivery:false },  SalesOrder:{}, fixedVals:{ ITPProcedure:[]... }, ActivityScope:[], itp:[tree] }
    */
 
-  (Controller, JSONModel, Filter, FilterOperator) =>
+  (Controller, JSONModel, Filter, FilterOperator, Popover, Button, BindingMode) =>
     Controller.extend("zproddoc.controller.Worklist", {
       onInit() {
         const oCtrl = this;
         const oDataModel = new JSONModel({});
         oDataModel.setProperty("/SalesOrder", {
-          SalesOrderID: "30000634",
-          SalesOrderItem: "10",
+          SalesOrderID: "0000079672",
+          SalesOrderItem: "001040",
         });
         oDataModel.setProperty("/ActivityScope", []);
+
         oDataModel.setProperty("dispOptions", {
           selectedOnly: false,
           isExtendedDelivery: false,
@@ -200,7 +204,6 @@ sap.ui.define(
       },
 
       onActivityChange(par1) {
-        debugger;
 
         const sKey = par1.getParameter("selectedItem").getProperty("key");
         const sPath = par1.getSource().getBindingInfo("items").binding.getContext().getPath();
@@ -209,9 +212,103 @@ sap.ui.define(
         const oActScope = oObject.ActivityScope
           .find((el) => el.Activity === sKey);
 
+        if(oActScope.IsSpecial){
+          debugger;
+
+          this.getView().getModel("data").setProperty( `${sPath}/ItpProcedureDescr` ,""  );
+          this.getView().getModel("data").setProperty( `${sPath}/AcceptCritDescr` ,""  );
+          this.getView().getModel("data").setProperty( `${sPath}/SpecialMode` , true  );
+
+          /*if (!this.mPopover) {
+
+            this.mPopover = new Popover({
+              content: [
+                new sap.m.TextArea({ value: "" } )
+              ],
+              beginButton: [ new Button({text: "save", press: this.popoverActionPress}) ],
+              showHeader: false
+            });
+
+          }
+          this.mPopover.openBy(par1.getSource());
+ */
+        }else{
           this.getView().getModel("data").setProperty( `${sPath}/ItpProcedureDescr` ,oActScope.ItpProcedureDescr  );
           this.getView().getModel("data").setProperty( `${sPath}/AcceptCritDescr` ,oActScope.AcceptCritDescr  );
+          this.getView().getModel("data").setProperty( `${sPath}/SpecialMode` , false  );
 
+        }
+
+
+      },
+
+      onProcedurePress(oEvent){
+        debugger;
+
+        var oCtx = oEvent.getSource().getBindingContext(),
+				oControl = oEvent.getSource();
+
+        const sPath = oEvent.getSource().getBindingInfo("visible").binding.getBindings()[0].getContext().getPath();
+				//oView = this.getView();
+
+        //const sKey = oEvent.getParameter("selectedItem").getProperty("key");
+        //const sPath = oEvent.getSource().getBindingInfo("items").binding.getContext().getPath();
+        //const oObject = this.getView().getModel("data").getObject(sPath);
+
+        const oTextArea = new sap.m.TextArea({
+          value: oEvent.getSource().getModel("data").getProperty(`${sPath}/ItpProcedureDescr`)
+        } );
+        // oTextArea.bindProperty("value",{ 
+        //   path: `${sPath}/ItpProcedureDescr`,
+        //   mode: BindingMode.TwoWay,
+        //   model: "data"
+        // });
+
+        if (true) {
+
+            this.mPopover = new Popover({
+              content: [
+                oTextArea
+              ],
+              beginButton: [ new Button({text: "save", press: this.popoverActionPress}) ],
+              showHeader: false
+            });
+
+          }
+          this.mPopover.openBy(oEvent.getSource());
+ 
+
+      },
+
+      onAccCriteriaPress(oEvent){
+        debugger;
+
+        var oCtx = oEvent.getSource().getBindingContext(),
+				oControl = oEvent.getSource();
+				//oView = this.getView();
+
+        //this.mPopover.close();
+
+      },
+
+      popoverActionPress(oEvent){       
+        var oCtx = oEvent.getSource().getBindingContext();
+				var oControl = oEvent.getSource();
+				//oView = this.getView();
+
+        let oButton = oControl.getParent().getParent().getParent()._oControl._oOpenBy;
+        let sValue = oControl.getParent().getParent().getContent()[0].getValue();
+        let sPath = oControl.getParent().getParent().getParent()._oControl._oOpenBy.getBindingInfo("visible").binding.getBindings(0)[0].getContext().getPath();
+        let oModel = oControl.getParent().getParent().getParent()._oControl._oOpenBy.getModel("data");
+
+        debugger;
+
+        oModel.setProperty(`${sPath}/ItpProcedureDescr`, sValue);
+
+
+        // this.getParent().getParent().getParent()._oControl._oOpenBy.getModel("data").getObject()
+
+        //this.mPopover.close();
 
       },
 
